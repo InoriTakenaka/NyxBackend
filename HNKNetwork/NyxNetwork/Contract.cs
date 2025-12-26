489592f62de079db574495d3e3c958e525a708f1
InoriTakenaka <19531669+InoriTakenaka@users.noreply.github.com>
2025-12-26 15:39:43 +0800

    feat(TcpConnections) - create TcpConnection class to implement a TCP Client.

---
+++ HNKNetwork/NyxNetwork/Contract.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NyxNetwork
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct ContractHeader
+    {
+        public const byte Header = 0x0F;
+        public ushort Length;
+        public ushort Method;
+    }
+
+    public enum Constant : ushort
+    {
+        HEALTHCHECK = 0x00,
+        ACK = 0x01,
+    }
+}
